2a34067ada0839cb3595114bccb95214936aa076
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-20 11:40:24 -0800

    add override for equality operator, add save method, add specs for both and add before and after blocks that clear test environment

--- lib/word.rb
+++ lib/word.rb
@@ -1,13 +1,20 @@
 class Word
+  attr_reader :id
   attr_accessor :name
 
-  def initialize(name)
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1
   end
 
-  @@words = []
+  @@words = {}
+  @@total_rows = 0
 
   def self.all()
-    @@words
+    @@words.values()
+  end
+
+  def ==(other_word)
+    self.name.eql?(other_word.name)
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -1,8 +1,24 @@
 require 'rspec'
 require 'word'
 
+describe '#Word' do
+  before(:each) do
+    Word.clear()
+  end
+end
+
 describe(".all") do
   it("is empty at first") do
-    expect(Word.all()).to(eq([]))
+    expect(Word.all()).to(eq({}))
+  end
+end
+
+describe('#save') do
+  it("saves a word") do
+    word = Word.new("embiggen", nil)
+    word.save()
+    word2 = Word.new("cromulent", nil)
+    word2.save()
+    expect(Word.all).to(eq([word, word2]))
   end
 end
