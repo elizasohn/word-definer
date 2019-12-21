231e4672c69fdf947bf41062bace461281658edd
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-20 16:44:25 -0800

    add new views for list of words and adding a word

--- app.rb
+++ app.rb
@@ -1,41 +1,46 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/song')
+require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 
-require('sinatra')
 
 get('/') do
-  "This will be our home page. '/' is always the root route in a Sinatra application."
+  @words = Word.all
+  erb(:words)
 end
 
-get('/albums') do
-  "This route will show a list of all albums."
+get('/words') do
+  @words = Word.all
+  erb(:words)
 end
 
-get('/albums/new') do
-  "This will take us to a page with a form for adding a new album."
+get('/words/new') do
+  erb(:new_word)
 end
 
-get('/albums/:id') do
-  "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
+get('/words/:id') do
+  "This route will show a specific word based on its ID. The value of ID here is #{params[:id]}."
 end
 
-post('/albums') do
-  "This route will add an album to our list of albums. We can't access this by typing in the URL. In a future lesson, we will use a form that specifies a POST action to reach this route."
+post('/words') do
+  name = params[:word_name]
+  word = Word.new(name, nil)
+  word.save()
+  @words = Word.all()
+  erb(:words)
 end
 
-get('/albums/:id/edit') do
-  "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
+get('/words/:id/edit') do
+  "This will take us to a page with a form for updating an word with an ID of #{params[:id]}."
 end
 
-patch('/albums/:id') do
-  "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
+patch('/words/:id') do
+  "This route will update an word. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
 end
 
-delete('/albums/:id') do
-  "This route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
+delete('/words/:id') do
+  "This route will delete an word. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
 end
 
 get('/custom_route') do
