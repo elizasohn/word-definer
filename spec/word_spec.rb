f028b3bf5cb78a41d3aa19ec2886bc73b6cb1a37
Eliza Sohn <eliza.sohn@gmail.com>
2019-12-25 11:30:58 -0800

    add failing spec to return a word's definitions

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -70,4 +70,16 @@ describe '#Word' do
       expect(Word.all).to(eq([word2]))
     end
   end
+
+  describe('#definitions') do
+    it("returns an words's definitions") do
+      word = Word.new("embiggen", nil)
+      word.save()
+      definition = Definition.new("to make bigger", word.id, nil)
+      definition.save()
+      definition2 = Definition.new("to give bigness", word.id, nil)
+      definition2.save()
+      expect(word.definitions).to(eq([definition, definition2]))
+    end
+  end
 end
