6857b8c790d78760dbb2623fe2afab2728688af1
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-05 14:15:19 -0800

    add nested routes, add definition.erb file

--- app.rb
+++ app.rb
@@ -1,9 +1,35 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
 
+get('/words/:id/definitions/:definition_id') do
+  @definition = Definition.find(params[:definition_id].to_i())
+  erb(:definition)
+end
+
+post('/words/:id/definitions') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+patch('/words/:id/definitions/:definition_id') do
+  @word = Word.find(params[:id].to_i())
+  definition = Definition.new(params[:definition_name], @word.id, nil)
+  definition.save()
+  erb(:word)
+end
+
+delete('/words/:id/definitions/:definition_id') do
+  definition = Definition.find(params[:definition_id].to_i())
+  definition.delete
+  @word = Word.find(params[:id].to_i())
+  erb(:album)
+end
 
 get('/') do
   @words = Word.all
