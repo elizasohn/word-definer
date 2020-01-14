4a27df0c0351eb08710f2f3c4b974c46ce144810
Eliza Sohn <eliza.sohn@gmail.com>
2020-01-13 19:01:03 -0800

    fix initialize parameter

--- lib/definition.rb
+++ lib/definition.rb
@@ -5,7 +5,7 @@ class Definition
   @@definitions = {}
   @@total_rows = 0
 
-  def initialize(definition, word_id, id)
+  def initialize(name, word_id, id)
     @name = name
     @word_id = word_id
     @id = id || @@total_rows += 1
